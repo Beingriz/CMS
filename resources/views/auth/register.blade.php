b2efb1a40a92a3a5f95fb124b85049431c752cea
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2022-07-06 14:28:26 +0530

    Login and Register Pages Done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,8 +107,7 @@
         <script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
         <script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
         <script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
-
-        <script src="assets/js/app.js"></script>
+        <script src="{{asset('backend/assets/js/app.js')}}"></script>
 
     </body>
 </html>
