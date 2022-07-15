676ea557c790635bbc75cf078babf025be0c15ba
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2022-07-15 16:55:38 +0530

    working on Dynamic
    Dashboard

--- resources/views/livewire/dynamic-dashboard.blade.php
+++ resources/views/livewire/dynamic-dashboard.blade.php
@@ -50,14 +50,14 @@
             @endforeach
     </div>
 
+    <div class="row">
+        <div class="dynamic-table-header">
+            <p class="dynamic-heading"> Status of {{$Serv_Name}} , {{$Sub_Serv_Name}} | Status Board</p>
+        </div>
+    </div>
 
-
-            <div class="dynamic-table-header">
-                <p class="dynamic-heading"> Status of {{$Serv_Name}} , {{$Sub_Serv_Name}} | Status Board</p>
-            </div>
-
-            <div class="row">
-            @if ($temp_count>0)
+    <div class="row">
+        @if ($temp_count>0)
             @foreach ($status as $item)
             <a href="#" class="col-xl-3 col-md-10" wire:click.prevent="ShowDetails('{{$item->Status}}')">
             <div class="card">
@@ -77,266 +77,116 @@
                 </div>
             </a>
             @endforeach
-            @endif
-        </div>
+        @endif
+    </div>
 
 {{-- ------------------------------------------------------------------------------------ --}}
-@if ($count>0)
-<div class="row">
-    @if (!empty($StatusDetails))
-    <div class="col-lg-12">
-    <div class="card">
-        <div class="card-body">
-            <h4 class="card-title">Applicaiton with Deatails of {{$Sub_Serv_Name}} ,{{$status_name}}  </h4>
-            {{-- <p class="card-title-desc">Add <code>.table-bordered</code> for borders on all sides of the table and cells.</p> --}}
+    <div class="row">
+    @if ($count>0)
+        @if (!empty($StatusDetails))
+            <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Applicaiton with Deatails of {{$Sub_Serv_Name}} ,{{$status_name}}  </h4>
+                    {{-- <p class="card-title-desc">Add <code>.table-bordered</code> for borders on all sides of the table and cells.</p> --}}
+                    <div class="table-responsive">
+                        <table class="table table-bordered mb-0">
 
-            <div class="table-responsive">
-                <table class="table table-bordered mb-0">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th >Date</th>
+                                    <th>Name</th>
+                                    <th>Mobile No</th>
+                                    <th>Application</th>
+                                    <th>Service Type</th>
+                                    <th>Ack. No</th>
+                                    <th>Total</th>
+                                    <th>Paid</th>
+                                    <th>Balance &#x20B9;</th>
+                                    <th>Change Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($StatusDetails as $data)
+                                <tr>
+                                    <td>
+                                        {{ $n++ }}
+                                    </td>
+                                    <td>{{ $data->Received_Date }}</td>
+                                    <td>{{ $data->Name }}</td>
+                                    <td>{{ $data->Mobile_No }}</td>
+                                    <td>{{ $data->Application }}</td>
+                                    <td>
+                                        <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateServiceType('{{$data->Id}}','{{$data->Application_Type}}',$event.target.value)">
+                                            <option value="{{ $data->Application_Type }}">{{ $data->Application_Type }}</option>
+                                            @foreach ($SubServices as $item)
+                                                <option value="{{$item->Name}}">{{$item->Name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td>{{ $data->Ack_No }}</td>
+                                    <td>{{ $data->Total_Amount }}</td>
+                                    <td>{{ $data->Amount_Paid }}</td>
 
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th >Date</th>
-                            <th>Name</th>
-                            <th>Mobile No</th>
-                            <th>Application</th>
-                            <th>Service Type</th>
-                            <th>Ack. No</th>
-                            <th>Total</th>
-                            <th>Paid</th>
-                            <th>Balance &#x20B9;</th>
-                            <th>Change Status</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($StatusDetails as $data)
-                        <tr>
-                            <td>
-                                {{ $n++ }}
-                            </td>
-                            <td  >{{ $data->Received_Date }}</td>
-                            <td   >{{ $data->Name }}</td>
-                            <td   >{{ $data->Mobile_No }}</td>
-                            <td   >{{ $data->Application }}</td>
-                            <td   >
-                                <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateServiceType('{{$data->Id}}','{{$data->Application_Type}}',$event.target.value)">
-                                    <option value="{{ $data->Application_Type }}">{{ $data->Application_Type }}</option>
-                                    @foreach ($SubServices as $item)
-                                        <option value="{{$item->Name}}">{{$item->Name}}</option>
-                                    @endforeach
-                                </select>
-                            </td>
-                            <td   >{{ $data->Ack_No }}</td>
-                            <td   >{{ $data->Total_Amount }}</td>
-                            <td   >{{ $data->Amount_Paid }}</td>
+                                    <td>{{ $data->Balance }}</td>
+                                    <td>
+                                        <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateStatus('{{$data->Id}}','{{$data->Status}}',$event.target.value)">
+                                            <option value="{{ $data->Status }}">{{ $data->Status }}</option>
+                                            @foreach ($status as $status_list)
+                                                <option value="{{$status_list->Status}}">{{$status_list->Status}}</option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td>
 
-                            <td   >{{ $data->Balance }}</td>
-                            <td   >
-                                <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateStatus('{{$data->Id}}','{{$data->Status}}',$event.target.value)">
-                                    <option value="{{ $data->Status }}">{{ $data->Status }}</option>
-                                    @foreach ($status as $status_list)
-                                        <option value="{{$status_list->Status}}">{{$status_list->Status}}</option>
-                                    @endforeach
-                                </select>
-                            </td>
-                            <td   >
+                                        <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                                            <div class="btn-group" role="group">
+                                                <button id="btnGroupVerticalDrop1" type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    Action <i class="mdi mdi-chevron-down"></i>
+                                                </button>
+                                                <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop1" style="">
+                                                    <a class="dropdown-item" href='/digital/cyber/open_app/{{ $data->Id }}'>Open</a>
+                                                    <a class="dropdown-item" href='/digital/cyber/edit_app/{{ $data->Id }}'>Edit</a>
+                                                </div>
+                                            </div>
 
-                                <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
-                                    <div class="btn-group" role="group">
-                                        <button id="btnGroupVerticalDrop1" type="button" class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Action <i class="mdi mdi-chevron-down"></i>
-                                        </button>
-                                        <div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop1" style="">
-                                            <a class="dropdown-item" href='/digital/cyber/open_app/{{ $data->Id }}'>Open</a>
-                                            <a class="dropdown-item" href='/digital/cyber/edit_app/{{ $data->Id }}'>Edit</a>
                                         </div>
-                                    </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                            @endif
+                        @endif
+                        </table>
+                    </div>
 
-                                </div>
-                            </td>
-
-                        </tr>
-
-                        @endforeach
-                    </tbody>
-                    @endif
                 </div>
-                @endif
-                </table>
             </div>
-
         </div>
-    </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{{-- ------------------------------ --}}
-            {{-- @if ($count>0)
-            <div class="row">
-                @if (!empty($StatusDetails))
-                    <div class="row"></div>
-                    <div class="table-header">
-                        <p class="heading2">Applicaiton with Deatails of {{$Sub_Serv_Name}} ,{{$status_name}}  </p>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th >Sl.No</th>
-                                <th >Date</th>
-                                <th>Name</th>
-                                <th>Mobile No</th>
-                                <th>Application</th>
-                                <th>Service Type</th>
-                                <th>Ack. No</th>
-                                <th>Total</th>
-                                <th>Paid</th>
-                                <th>Balance &#x20B9;</th>
-                                <th>Change Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($StatusDetails as $data)
-                            <tr>
-                                <td   >
-                                    {{ $n++ }}
-                                </td>
-                                <td   >{{ $data->Received_Date }}</td>
-                                <td   >{{ $data->Name }}</td>
-                                <td   >{{ $data->Mobile_No }}</td>
-                                <td   >{{ $data->Application }}</td>
-                                <td   >
-                                    <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateServiceType('{{$data->Id}}','{{$data->Application_Type}}',$event.target.value)">
-                                        <option value="{{ $data->Application_Type }}">{{ $data->Application_Type }}</option>
-                                        @foreach ($SubServices as $item)
-                                            <option value="{{$item->Name}}">{{$item->Name}}</option>
-                                        @endforeach
-                                    </select>
-                                </td>
-                                <td   >{{ $data->Ack_No }}</td>
-                                <td   >{{ $data->Total_Amount }}</td>
-                                <td   >{{ $data->Amount_Paid }}</td>
-
-                                <td   >{{ $data->Balance }}</td>
-                                <td   >
-                                    <select name="ChangeStatus" id="ChangeStatus" class="form-control-sm form-control" wire:change="UpdateStatus('{{$data->Id}}','{{$data->Status}}',$event.target.value)">
-                                        <option value="{{ $data->Status }}">{{ $data->Status }}</option>
-                                        @foreach ($status as $status_list)
-                                            <option value="{{$status_list->Status}}">{{$status_list->Status}}</option>
-                                        @endforeach
-                                    </select>
-                                </td>
-                                <td   >
-                                    <div class="btn-group" role="group"
-                                        aria-label="Button group with nested dropdown">
-                                        <div class="btn-group btn-group-sm " role="group">
-                                            <button id="btnGroupDrop1" type="button"
-                                                class="btn btn-info dropdown-toggle" data-mdb-toggle="dropdown"
-                                                aria-expanded="false">
-                                                Edit
-                                            </button>
-                                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                                                <li><a class="dropdown-item "
-                                                        href='/digital/cyber/open_app/{{ $data->Id }}'>Open</a>
-                                                </li>
-                                                <li><a class="dropdown-item "
-                                                        href='/digital/cyber/edit_app/{{ $data->Id }}'>Edit</a>
-                                                </li>
-
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </td>
-
-                            </tr>
-
-                            @endforeach
-                        </tbody>
-                    </table>
-                @endif
-            </div>
-            @endif --}}
-
-
-
-
-
-
-            {{-- Applicaiton Insight --}}
-            <div class="dashboard-insight">
-                <div class="right-menu-section">
-                    <img src="\digital/cyber/photo_gallery\insight.jpg" alt="" >
-                    <div class="sec-data">
-                        <p class="section-heading">Prime Service</p>
-                        <p class="section-value">Service Name</p>
-                        <p class="section-pre-values">Application Count </p>
-                    </div>
-                </div>
-                <div class="right-menu-section">
-                    <img src="\digital/cyber/photo_gallery\insight.jpg" alt="" >
-                    <div class="sec-data">
-                        <p class="section-heading">Montly Revenue</p>
-                        <p class="section-value">Total Amount</p>
-                        <p class="section-pre-values">Balance Due</p>
-                    </div>
-                </div>
-                <div class="right-menu-section">
-                    <img src="\digital/cyber/photo_gallery\insight.jpg" alt="" >
-                    <div class="sec-data">
-                        <p class="section-heading">Delivered</p>
-                        <p class="section-value">Total</p>
-                        <p class="section-pre-values">This Month</p>
-                    </div>
-                </div>
-                <div class="right-menu-section">
-                    <img src="\digital/cyber/photo_gallery\insight.jpg" alt="" >
-                    <div class="sec-data">
-                        <p class="section-heading">Pending</p>
-                        <p class="section-value">Total </p>
-                        <p class="section-pre-values">This Month</span></p>
-                    </div>
-                </div>
-
-            </div>
-            <div class="row"></div>
+{{-- ../{{$bookmark->Thumbnail}} --}}
             {{-- Application Book Marks BookMarks --}}
-            @if (count($bookmarks)>0)
-            <div class="data-table-header">
-                <p class="heading">Bookmarks for {{$Serv_Name}} </p>
-            </div>
-            <div class="row"></div>
-            <div class="border">
-                <div class="bookmark-container">
 
-                    @foreach($bookmarks as $bookmark)
-                    <a href="{{ $bookmark->Hyperlink }}" target="_blank" class="bookmark">
-                        <img class="b-img" src="../{{$bookmark->Thumbnail}}" alt="Bookmark Icon">
-                        <p class="b-name" >{{$bookmark->Name}}</p>
-                    </a>
-                    @endforeach
-                </div>
-            </div>
-            @endif
+        <div class="data-table-header">
+            <p class="heading">Bookmarks for {{$Serv_Name}} </p>
         </div>
+        @if (count($bookmarks)>0)
+            <div class="row">
+                @foreach($bookmarks as $bookmark)
+                <a  href="{{ $bookmark->Hyperlink }}" class="col-md-5 col-xl-1">
 
+                    <!-- Simple card -->
+                    <div class="card">
+                        <img class="card-img-top img-fluid" src="{{asset('backend/assets/images/small/img-1.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-title">{{$bookmark->Name}}</p>
+                        </div>
+                    </div>
+                </a>
+                @endforeach
+            </div>
+        @endif
+            <div class="row"></div>
+</div>
 </div>
