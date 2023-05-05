1e2daa242bd45a1755cd2b27afa2ccf55004f8e5
Md Rizwan <68804148+Beingriz@users.noreply.github.com>
2023-05-05 09:21:18 +0530

    New Design for Login, Register & Forgot Password
    All 3 Pages with new Design working,

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,130 +1,67 @@
-<!doctype html>
-<html lang="en">
+@extends('auth.auth_master')
+@section('auth')
+<section class="h-100 gradient-form" style="background-color: #eee;">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-xl-10">
+          <div class="card rounded-3 text-black">
+            <div class="row g-0">
+              <div class="col-lg-6">
+                <div class="card-body p-md-5 mx-md-4">
 
-    <head>
+                  <div class="text-center">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                      style="width: 185px;" alt="logo">
+                    <h4 class="mt-1 mb-5 pb-1">Welcome to Digital Cyber</h4>
+                  </div>
+                  <!-- Session Status -->
+                  <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <meta charset="utf-8" />
-        <title>Login | Admin</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesdesign" name="author" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
+                  <!-- Validation Errors -->
+                  <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                  <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <p>Please login to your account</p>
 
-        <!-- Bootstrap Css -->
-        <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
-
-
-    </head>
-
-    <body class="auth-body-bg">
-        <div class="bg-overlay"></div>
-        <div class="wrapper-page">
-            <div class="container-fluid p-0">
-                <div class="card">
-                    <div class="card-body">
-
-                        <div class="text-center mt-4">
-                            <div class="mb-3">
-                                <a href="index.html" class="auth-logo">
-                                    <img src="{{asset('backend/assets/images/dc-logo.png')}}" height="30" class="logo-dark mx-auto" alt="">
-                                    <img src="{{asset('backend/assets/images/logo-light.png')}}" height="30" class="logo-light mx-auto" alt="">
-                                </a>
-                            </div>
-                        </div>
-
-                        <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
-                        <!-- Session Status -->
-                        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-                        <!-- Validation Errors -->
-                        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-                        <div class="p-3">
-                           <form class="form-horizontal mt-3" method="POST" action="{{ route('login') }}">
-                                    @csrf
-                                <div class="form-group mb-3 row">
-                                    <div class="col-12">
-                                        <input  id="username"  name="username" class="form-control" type="text" required autofocus placeholder="Username">
-                                    </div>
-                                </div>
-
-                                <div class="form-group mb-3 row">
-                                    <div class="col-12">
-                                        <input id="password"  name="password" class="form-control" type="password" required="" placeholder="Password">
-                                    </div>
-                                </div>
-
-                                <div class="form-group mb-3 row">
-                                    <div class="col-12">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                            <label class="form-label ms-1" for="customCheck1">Remember me</label>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="form-group mb-3 text-center row mt-3 pt-1">
-                                    <div class="col-12">
-                                        <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Log In</button>
-                                    </div>
-                                </div>
-
-                                <div class="form-group mb-0 row mt-2">
-                                    <div class="col-sm-7 mt-3">
-                                        <a href="{{ route('password.request')}}" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
-                                    </div>
-                                    <div class="col-sm-5 mt-3">
-                                        <a href="{{ route('register')}}" class="text-muted"><i class="mdi mdi-account-circle"></i> Create an account</a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <!-- end -->
+                    <div class="form-outline mb-4">
+                      <input type="text" id="form2Example11" name="username" class="form-control"
+                        placeholder="Username" />
+                      <label class="form-label" for="form2Example11">Username</label>
                     </div>
-                    <!-- end cardbody -->
+
+                    <div class="form-outline mb-4">
+                      <input type="password" id="form2Example22" name="password" class="form-control" />
+                      <label class="form-label" for="form2Example22">Password</label>
+                    </div>
+
+                    <div class="text-center pt-1 mb-5 pb-1">
+                      <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Log
+                        in</button>
+                      <a class="text-muted" href="{{route('password.request')}}">Forgot password?</a>
+                      <a class="text-primary" href="{{route('home')}}">Home Page</a>
+                    </div>
+
+                    <div class="d-flex align-items-center justify-content-center pb-4">
+                      <p class="mb-0 me-2">Don't have an account?</p>
+                      <a href="{{route('register')}}"  class="btn btn-outline-danger">Create new</a>
+                    </div>
+
+                  </form>
+
                 </div>
-                <!-- end card -->
+              </div>
+              <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                  <h4 class="mb-4">We are more than just a company</h4>
+                  <p class="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+              </div>
             </div>
-            <!-- end container -->
+          </div>
         </div>
-        <!-- end -->
-
-        <!-- JAVASCRIPT -->
-        <script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
-        <script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-        <script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
-        <script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
-        <script src="{{asset('backend/assets/js/app.js')}}"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-<script>
- @if(Session::has('message'))
- var type = "{{ Session::get('alert-type','info') }}"
- switch(type){
-    case 'info':
-    toastr.info(" {{ Session::get('message') }} ");
-    break;
-
-    case 'success':
-    toastr.success(" {{ Session::get('message') }} ");
-    break;
-
-    case 'warning':
-    toastr.warning(" {{ Session::get('message') }} ");
-    break;
-
-    case 'error':
-    toastr.error(" {{ Session::get('message') }} ");
-    break;
- }
- @endif
-</script>
-
-    </body>
-</html>
+      </div>
+    </div>
+  </section>
+@endsection
